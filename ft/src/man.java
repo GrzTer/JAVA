aee7f900a21135a593654eb4989d5dedb5ca739c
DevGreg <grzegorz.tereszkiewicz@infotech.edu.pl>
2025-03-14 19:27:37 +0100

    ..........

---
+++ ft/src/man.java
@@ -0,0 +1,38 @@
+//public class man {
+//    public static String iGregory() {
+//        return "Hello, my name is Gregory";
+//    }
+//}
+//public class man {
+//    public static void someMethod() {
+//        int h = 1;
+//        if (h == 0) {
+//            System.out.println("Equal: 0");
+//        } else if (h == 1) {
+//            System.out.println("Equal: 1");
+//        } else {
+//            System.out.println("Not equal");
+//        }
+//    }
+//}
+
+//public class man {
+//    public static void someMethod() {
+//        int h = 0;
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(i);
+//
+//        }
+//        while(h < 10) {
+//            System.out.println("hui"+h);
+//            h++;
+//        }
+//    }
+//}
+public class man {
+    public static void someMethod() {
+        String input = System.console().readLine();
+        for (int i =)
+        }
+    }
+}
